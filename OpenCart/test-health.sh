dac0875b8b3820c14dbe62a097397ff7d47d4765
Chris Paggen <cpaggen@cisco.com>
2017-09-20 14:08:48 +0200

    health checks script

--- OpenCart/test-health.sh
+++ OpenCart/test-health.sh
@@ -5,28 +5,31 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 printf "Running POD tests\n"
-for i in {25..45..4}
-    do
-       echo "Testing haproxy 10.48.58.$i"
-       wget --quiet --timeout 3 10.48.58.$i
-       if [ -f "index.html" ]
-       then
-          printf "${GREEN}POD 10.48.58.$i works!\n${NC}"
-          rm index.html
-       else
-	  printf "${RED}POD malfunction!\n${NC}"
-       fi
-done
 
-for i in {69..84..4}
+test_pod()
+{
+for ((i = $1; i <= $2; i+=4))
     do
        echo "Testing haproxy 10.48.58.$i"
        wget --quiet --timeout 3 10.48.58.$i
+       db=$(($i+1))
+       maria=`nc 10.48.58.$db 3306 -w 1`
+       if [[ $maria == *"MariaDB"* ]]
+       then
+          printf "${GREEN}DB is up!\n${NC}"
+       else
+          printf "${RED}DB is down!\n${NC}"
+       fi
        if [ -f "index.html" ]
        then
-          printf "${GREEN}POD 10.48.58.$i works!\n${NC}"
+          printf "${GREEN}Haproxy 10.48.58.$i works!\n${NC}"
           rm index.html
        else
 	  printf "${RED}POD malfunction!\n${NC}"
        fi
 done
+}
+
+test_pod 25 45
+test_pod 69 84
+
