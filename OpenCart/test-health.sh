54f099456dfef19ed0b19f8566eb5e7d7000c1bc
Chris Paggen <cpaggen@cisco.com>
2017-09-20 14:26:00 +0200

    refactored health check script

--- OpenCart/test-health.sh
+++ OpenCart/test-health.sh
@@ -4,29 +4,29 @@ RED='\033[0;31m'
 GREEN='\033[0;32m' 
 NC='\033[0m'
 
+rm index.html
 printf "Running POD tests\n"
 
 test_pod()
 {
 for ((i = $1; i <= $2; i+=4))
     do
-       echo "Testing haproxy 10.48.58.$i"
-       wget --quiet --timeout 3 10.48.58.$i
-       db=$(($i+1))
-       maria=`nc 10.48.58.$db 3306 -w 1`
+       haproxy=10.48.58.$i
+       db=10.48.58.$(($i+1))
+       web1=10.48.58.$(($i+2))
+       web2=10.48.58.$(($i+3))
+       wget --quiet --timeout 3 $haproxy && printf "${GREEN}Haproxy $haproxy works!\n${NC}" || printf "${RED}POD malfunction!\n${NC}"
+       wget --quiet $web1 --timeout 3 && printf "web1 $web1 up\n" || printf "${RED}web1 $web1 down${NC}\n"
+       wget --quiet $web2 --timeout 3 && printf "web2 $web2 up\n" || printf "${RED}web2 $web2 down${NC}\n"
+       rm index.html.*
+       maria=`nc $db 3306 -w 1`
        if [[ $maria == *"MariaDB"* ]]
        then
-          printf "${GREEN}DB is up!\n${NC}"
+          printf "${GREEN}DB $db is up!\n${NC}"
        else
-          printf "${RED}DB is down!\n${NC}"
+          printf "${RED}DB $db is down!\n${NC}"
        fi
-       if [ -f "index.html" ]
-       then
-          printf "${GREEN}Haproxy 10.48.58.$i works!\n${NC}"
-          rm index.html
-       else
-	  printf "${RED}POD malfunction!\n${NC}"
-       fi
+       printf "========\n"
 done
 }
 
